4d71a82f9eca87dafbebf76c507e6096cf56e253
Ivar Stangeby <istangeby@gmail.com>
2016-04-29 16:26:11 +0200

    node blowup done

--- project/visualization/curves/curves.py
+++ project/visualization/curves/curves.py
@@ -6,16 +6,18 @@ from numpy import meshgrid
 matplotlib.pyplot.xkcd()
 
 delta = 0.025
-xrange = arange(-2.5, 5.0, delta)
+xrange = arange(-5, 5.0, delta)
 yrange = arange(-2.5, 2.5, delta)
 X, Y = meshgrid(xrange,yrange)
 
 # F is one side of the equation, G is the other
-F = Y**2
-G = X**2 + X**3 
+# F = X**2 * (Y**2 - 1 - X)
+F = X**2*(1 - Y**2 - X*Y**3)
+G = 0
 
-matplotlib.pyplot.axis('off')
+matplotlib.pyplot.axis('on')
+matplotlib.pyplot.grid('on')
 matplotlib.pyplot.contour(X, Y, (F - G), [0])
 
-matplotlib.pyplot.savefig('double_point.pdf')
+matplotlib.pyplot.savefig('case_1.pdf')
 
